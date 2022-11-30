e02659229ec70f20b7047f86580a340e8bd80605
leecs99 <msi991030@naver.com>
2022-11-30 23:10:12 +0900

    folder upload

---
+++ Login/A/src/core/theme.js
@@ -0,0 +1,14 @@
+// 주제
+import { DefaultTheme } from 'react-native-paper'
+
+export const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    text: '#000000',
+    primary: '#121330',
+    secondary: '#414757',
+    error: '#f13a59', //오류시 텍스트 색 -> 빨간계열
+    success: '#00B386',
+  },
+}
---
+++ Login/A/src/helpers/nameValidator.js
@@ -0,0 +1,6 @@
+// 이름 자리가 비어있다.
+
+export function nameValidator(name) {
+  if (!name) return "Name can't be empty."
+  return ''
+}
